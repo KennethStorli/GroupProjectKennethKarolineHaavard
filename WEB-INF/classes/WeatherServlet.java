4272530d8b14457779ccb780cbfd6ccf2d793a02
Kenneth Storli <kenneth.storli@gmail.com>
2018-09-18 14:43:33 +0200

    Chore: Showing how to use git

--- WEB-INF/classes/WeatherServlet.java
+++ WEB-INF/classes/WeatherServlet.java
@@ -3,7 +3,7 @@ public class WeatherServlet extends HttpServlet {
    @Override
    public void doGet(HttpServletRequest request, HttpServletResponse response)
          throws IOException, ServletException{
-
+         	//here is a comment
     }
 
 
