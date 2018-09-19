3e30dfb3dc2cc54a66eb78223fb5978e19520e26
karolinr9 <klrykkelid@outlook.com>
2018-09-19 11:29:45 +0200

    remove unused import json and Date add Google api key in the right way

--- WEB-INF/classes/WeatherServlet.java
+++ WEB-INF/classes/WeatherServlet.java
@@ -1,37 +1,35 @@
 import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
-import org.json.JSONArray;
-import org.json.JSONObject;
-import java.util.Date;
 
 
 public class WeatherServlet extends HttpServlet {
 
-PrintWriter out;
+	PrintWriter out;
+	public static final String GOOGLE_API_KEY = "AIzaSyD4Ydc-Uq0N1Tnl9bQZFI0mhK3vcko_5Yk";
 
-   @Override
-   public void doGet(HttpServletRequest request, HttpServletResponse response)
-         throws IOException, ServletException{
-   	//api key: AIzaSyD4Ydc-Uq0N1Tnl9bQZFI0mhK3vcko_5Yk
 
-	   public static final String GOOGLE_API_KEY = AIzaSyD4Ydc-Uq0N1Tnl9bQZFI0mhK3vcko_5Yk;
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
+		//api key: AIzaSyD4Ydc-Uq0N1Tnl9bQZFI0mhK3vcko_5Yk
+
 
 		out = response.getWriter();
-         try {
-         	out.println("<html>");
-         	out.println("<head><title>Weather App</title><style>#map {height: 400px;  width: 100%;  }</style></head>");
-         	out.println("<body>");
-         	out.println("<div id=\"map\"></div><script>function initMap() {var uluru = {lat: -25.344, lng: 131.036};var map = new google.maps.Map(document.getElementById('map'), {zoom: 4, center: uluru});var marker = new google.maps.Marker({position: uluru, map: map});}</script><script async defer src=\"https://maps.googleapis.com/maps/api/js?key=" + GOOGLE_API_KEY + "&callback=initMap\"></script>");
+		try {
+			out.println("<html>");
+			out.println("<head><title>Weather App</title><style>#map {height: 400px;  width: 100%;  }</style></head>");
+			out.println("<body>");
+			out.println("<div id=\"map\"></div><script>function initMap() {var uluru = {lat: -25.344, lng: 131.036};var map = new google.maps.Map(document.getElementById('map'), {zoom: 4, center: uluru});var marker = new google.maps.Marker({position: uluru, map: map});}</script><script async defer src=\"https://maps.googleapis.com/maps/api/js?key=" + GOOGLE_API_KEY + "&callback=initMap\"></script>");
 
-         	out.println("<h1>Winter is coming</h1>"); 
+			out.println("<h1>Winter is coming</h1>");
 
-         	out.println("</body></html>");
+			out.println("</body></html>");
 
-         }finally {
-         	out.close();  // Always close the output writer
-      	}
-    }
+		} finally {
+			out.close();  // Always close the output writer
+		}
+	}
 
 
 }
