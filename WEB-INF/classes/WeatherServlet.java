43810fc292c32e22c51ae6e4756db3565092e98a
karolinr9 <klrykkelid@outlook.com>
2018-09-19 11:18:53 +0200

    add Google api key

--- WEB-INF/classes/WeatherServlet.java
+++ WEB-INF/classes/WeatherServlet.java
@@ -13,13 +13,16 @@ PrintWriter out;
    @Override
    public void doGet(HttpServletRequest request, HttpServletResponse response)
          throws IOException, ServletException{
+   	//api key: AIzaSyD4Ydc-Uq0N1Tnl9bQZFI0mhK3vcko_5Yk
+
+	   public static final String GOOGLE_API_KEY = AIzaSyD4Ydc-Uq0N1Tnl9bQZFI0mhK3vcko_5Yk;
 
 		out = response.getWriter();
          try {
          	out.println("<html>");
          	out.println("<head><title>Weather App</title><style>#map {height: 400px;  width: 100%;  }</style></head>");
          	out.println("<body>");
-         	out.println("<div id=\"map\"></div><script>function initMap() {var uluru = {lat: -25.344, lng: 131.036};var map = new google.maps.Map(document.getElementById('map'), {zoom: 4, center: uluru});var marker = new google.maps.Marker({position: uluru, map: map});}</script><script async defer src=\"https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap\"></script>");
+         	out.println("<div id=\"map\"></div><script>function initMap() {var uluru = {lat: -25.344, lng: 131.036};var map = new google.maps.Map(document.getElementById('map'), {zoom: 4, center: uluru});var marker = new google.maps.Marker({position: uluru, map: map});}</script><script async defer src=\"https://maps.googleapis.com/maps/api/js?key=" + GOOGLE_API_KEY + "&callback=initMap\"></script>");
 
          	out.println("<h1>Winter is coming</h1>"); 
 
@@ -29,8 +32,6 @@ PrintWriter out;
          	out.close();  // Always close the output writer
       	}
     }
-
-
 
 
 }
