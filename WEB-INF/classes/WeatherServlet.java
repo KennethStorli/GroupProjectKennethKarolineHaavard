761bbaf7f562fdeab2efaf8c567a886f8bc15a0e
karolinr9 <klrykkelid@outlook.com>
2018-09-19 11:53:56 +0200

    add back import json

--- WEB-INF/classes/WeatherServlet.java
+++ WEB-INF/classes/WeatherServlet.java
@@ -1,6 +1,8 @@
 import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 
 public class WeatherServlet extends HttpServlet {
@@ -20,7 +22,7 @@ public class WeatherServlet extends HttpServlet {
 			out.println("<html>");
 			out.println("<head><title>Weather App</title><style>#map {height: 400px;  width: 100%;  }</style></head>");
 			out.println("<body>");
-			out.println("<div id=\"map\"></div><script>function initMap() {var uluru = {lat: -25.344, lng: 131.036};var map = new google.maps.Map(document.getElementById('map'), {zoom: 4, center: uluru});var marker = new google.maps.Marker({position: uluru, map: map});}</script><script async defer src=\"https://maps.googleapis.com/maps/api/js?key=" + GOOGLE_API_KEY + "&callback=initMap\"></script>");
+			//out.println("<div id=\"map\"></div><script>function initMap() {var uluru = {lat: -25.344, lng: 131.036};var map = new google.maps.Map(document.getElementById('map'), {zoom: 4, center: uluru});var marker = new google.maps.Marker({position: uluru, map: map});}</script><script async defer src=\"https://maps.googleapis.com/maps/api/js?key=" + GOOGLE_API_KEY + "&callback=initMap\"></script>");
 
 			out.println("<h1>Winter is coming</h1>");
 
